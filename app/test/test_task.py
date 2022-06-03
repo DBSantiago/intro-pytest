28c9b66305c6d00cd203b918573856081da7c211
DBSantiago <deblasissantiago@gmail.com>
2022-06-03 19:52:21 -0300

    ADD: Pytest markers: due_date, errors, news

--- app/test/test_task.py
+++ app/test/test_task.py
@@ -6,9 +6,11 @@ from app.task import Task, DueDateError
 
 class TestTask:
 
+    @pytest.mark.news
     def test_task(self):
         assert True
 
+    @pytest.mark.news
     def test_new_task(self):
         date = datetime.now() + timedelta(days=1)
         task = Task("Test this", "Testing new task", "dbsantiago", date)
@@ -18,7 +20,16 @@ class TestTask:
         assert task.assigned_to == "dbsantiago"
         assert task.due_date == date
 
+    @pytest.mark.due_date
+    @pytest.mark.errors
     def test_due_date_error(self):
         with pytest.raises(DueDateError):
             date = datetime.now() - timedelta(days=1)
             task = Task("Test this", "Testing new task", "dbsantiago", date)
+
+    @pytest.mark.due_date
+    def test_due_date(self):
+        date = datetime.now() + timedelta(days=1)
+        task = Task("Test this", "Testing new task", "dbsantiago", date)
+
+        assert date > datetime.now()
