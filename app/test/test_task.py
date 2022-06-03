2de9b1ed39aa78ec390df32426ad7ba3df19f886
DBSantiago <deblasissantiago@gmail.com>
2022-06-03 19:38:50 -0300

    ADD: Unit test for new task

--- app/test/test_task.py
+++ app/test/test_task.py
@@ -1,7 +1,19 @@
+from datetime import datetime
 import pytest
+
+from app.task import Task
 
 
 class TestTask:
 
     def test_task(self):
         assert True
+
+    def test_new_task(self):
+        date = datetime.now()
+        task = Task("Test this", "Testing new task", "dbsantiago", date)
+
+        assert task.title == "Test this"
+        assert task.description == "Testing new task"
+        assert task.assigned_to == "dbsantiago"
+        assert task.due_date == date
